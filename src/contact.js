f9c011ab3a2faacc6987ffea471773da7b0b3aea
Anthony Babka <anthonybabka@gmail.com>
2022-06-24 17:02:57 -0700

    adding to gh

---
+++ src/contact.js
@@ -0,0 +1,20 @@
+import './home.css'
+
+function Contact(){
+    return (
+        <div className='contacts'>
+        <h2>Reach out!</h2>
+        <h3>Even if it's just to say Hi!</h3>
+        <div>
+          <ul>
+            <li>Email</li>
+            <li>Linkdin</li>
+            <li>GitHub</li>
+            <li>Los Angeles CA</li>
+          </ul>
+        </div>
+      </div>
+    )
+}
+
+export default Contact;
---
+++ src/portfolio.js
@@ -0,0 +1,24 @@
+import './home.css';
+
+function Portfolio() {
+    return (
+        <div className='portfolio'>
+            <div>
+                <h2>Portfolio</h2>
+                    <div>
+                    <h3>These are some of my projects.. my babbies.. my pride and joys..</h3>
+                        <div className='babbies'>
+                            <div className='projects'></div>
+                            <div className='projects'></div>
+                            <div className='projects'></div>
+                            <div className='projects'></div>
+                            <div className='projects'></div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Portfolio;
